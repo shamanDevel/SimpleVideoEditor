7cc6acb3e2aa79b87353a569b64808367a1a77e6
Sebastian Weiß <sebastian.weiss_germering@web.de>
2017-01-01 14:42:46 +0100

    started with timeline panel

--- src/org/shaman/sve/TimelinePanel.java
+++ src/org/shaman/sve/TimelinePanel.java
@@ -5,19 +5,151 @@
  */
 package org.shaman.sve;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.util.logging.Logger;
+import javax.swing.JScrollPane;
+import javax.swing.event.TreeModelEvent;
+import javax.swing.event.TreeModelListener;
+import javax.swing.table.TableColumn;
+import javax.swing.undo.UndoableEditSupport;
+import org.jdesktop.swingx.JXTreeTable;
+import org.jdesktop.swingx.table.TableColumnExt;
+import org.jdesktop.swingx.treetable.AbstractTreeTableModel;
+import org.shaman.sve.model.Project;
+import org.shaman.sve.model.Resource;
+import org.shaman.sve.model.TimelineObject;
+
 /**
  *
  * @author Sebastian
  */
 public class TimelinePanel extends javax.swing.JPanel {
+	private static final Logger LOG = Logger.getLogger(TimelinePanel.class.getName());
 
+	private Project project;
+	private UndoableEditSupport undoSupport;
+	
+	private JXTreeTable table;
+	private TimelineTreeTableModel tableModel;
+	
 	/**
 	 * Creates new form TimelinePanel
 	 */
 	public TimelinePanel() {
 		initComponents();
+		
+		tableModel = new TimelineTreeTableModel();
+		table = new JXTreeTable(tableModel);
+//		table.setPreferredSize(new Dimension(400, 160));
+		table.setRootVisible(false);
+		timelinePanel.setLayout(new BorderLayout());
+		timelinePanel.add(new JScrollPane(table));
+		timelinePanel.setPreferredSize(new Dimension(400, 158));
+	}
+	
+	public void setProject(Project project) {
+		this.project = project;
+		//init timeline
+		tableModel.fireUpdate();
+		TableColumn c = table.getColumn(0);
+		c.setResizable(false);
+		int w = 100;
+		c.setMaxWidth(w);
+		c.setMinWidth(w);
+		c.setPreferredWidth(w);
+		c.setWidth(w);
+		
+		addButton.setEnabled(true);
 	}
 
+	public void setUndoSupport(UndoableEditSupport undoSupport) {
+		this.undoSupport = undoSupport;
+	}
+
+	private class TimelineTreeTableModel extends AbstractTreeTableModel {
+
+		public TimelineTreeTableModel() {
+			super(new Object());
+		}
+		
+		public void fireUpdate() {
+			for (TreeModelListener l : getTreeModelListeners()) {
+				l.treeStructureChanged(new TreeModelEvent(this, new Object[]{project}));
+			}
+		}
+		
+		@Override
+		public int getColumnCount() {
+			return 2;
+		}
+
+		@Override
+		public boolean isCellEditable(Object node, int column) {
+			return false;
+		}
+		
+		@Override
+		public Class<?> getColumnClass(int column) {
+			if (column == 0) {
+				return String.class;
+			} else if (column == 1) {
+				return Object.class;
+			} else {
+				return Object.class;
+			}
+		}
+
+		@Override
+		public String getColumnName(int column) {
+			if (column == 0) {
+				return "Object";
+			} else if (column == 1) {
+				return "Timeline";
+			} else {
+				return "";
+			}
+		}
+
+		@Override
+		public Object getValueAt(Object node, int column) {
+			if (node instanceof TimelineObject) {
+				TimelineObject to = (TimelineObject) node;
+				if (column == 0) {
+					return to.toString();
+				} else if (column == 1) {
+					return to; //timeline display
+				}
+			}
+			return null;
+		}
+
+		@Override
+		public Object getChild(Object parent, int index) {
+			if (parent == root && project != null) {
+				return project.getTimelineObjects().get(index);
+			}
+			return null;
+		}
+
+		@Override
+		public int getChildCount(Object parent) {
+			if (parent == root && project != null) {
+				return project.getTimelineObjects().size();
+			}
+			return 0;
+		}
+
+		@Override
+		public int getIndexOfChild(Object parent, Object child) {
+			if (parent == root && project != null) {
+				return project.getTimelineObjects().indexOf(child);
+			}
+			return -1;
+		}
+		
+	}
+	
 	/**
 	 * This method is called from within the constructor to initialize the form.
 	 * WARNING: Do NOT modify this code. The content of this method is always
@@ -27,22 +159,78 @@ public class TimelinePanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        addButton = new javax.swing.JButton();
+        removeButton = new javax.swing.JButton();
+        timelinePanel = new javax.swing.JPanel();
+
         setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         setMinimumSize(new java.awt.Dimension(400, 100));
+
+        addButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/org/shaman/sve/icons/plus16.png"))); // NOI18N
+        addButton.setToolTipText("ad a new object to the timeline");
+        addButton.setEnabled(false);
+        addButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                addButtonEvent(evt);
+            }
+        });
+
+        removeButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/org/shaman/sve/icons/minus16.png"))); // NOI18N
+        removeButton.setToolTipText("Remove the selected object");
+        removeButton.setEnabled(false);
+        removeButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                removeButtonEvent(evt);
+            }
+        });
+
+        javax.swing.GroupLayout timelinePanelLayout = new javax.swing.GroupLayout(timelinePanel);
+        timelinePanel.setLayout(timelinePanelLayout);
+        timelinePanelLayout.setHorizontalGroup(
+            timelinePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 580, Short.MAX_VALUE)
+        );
+        timelinePanelLayout.setVerticalGroup(
+            timelinePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 647, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(addButton)
+                    .addComponent(removeButton))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(timelinePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 156, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(addButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(removeButton)
+                .addContainerGap(88, Short.MAX_VALUE))
+            .addComponent(timelinePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void addButtonEvent(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addButtonEvent
+        // TODO add your handling code here:
+    }//GEN-LAST:event_addButtonEvent
+
+    private void removeButtonEvent(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_removeButtonEvent
+        // TODO add your handling code here:
+    }//GEN-LAST:event_removeButtonEvent
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton addButton;
+    private javax.swing.JButton removeButton;
+    private javax.swing.JPanel timelinePanel;
     // End of variables declaration//GEN-END:variables
 }
